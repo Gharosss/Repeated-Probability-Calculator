af05a462a775e423ef363ed2107ae719e96f13c6
Yetkin Bilge Çınar <yetkinbilgecinar@hotmail.com>
2024-09-04 14:10:21 +0300

    feat: Improved Representation for Extreme Values

--- components.py
+++ components.py
@@ -17,10 +17,15 @@ def represent_in_words(number):
     (10**24, "septillion"),
     (10**27, "octillion"),
     (10**30, "nonillion"),
+    (10**33, "decillion")
     ]
-    
     # Determine the magnitude of the number and return its most significant part
-    if(number > 1000): 
+    if(number >= 1E36):
+        power_of_ten = len(str(number)) - 1
+        most_significant_part  = number // 10 ** power_of_ten
+        word_representation = f"{most_significant_part}E{str(power_of_ten)}"
+        print(f"Word Representation: {word_representation}")
+    elif(number >= 1E3): 
         for magnitude, name in reversed(magnitudes):
             if number >= magnitude:
                 most_significant_part  = number // magnitude
@@ -29,3 +34,16 @@ def represent_in_words(number):
     else:
         word_representation = f"{number}" # If the number is less than 1000 write it the same way
     return word_representation
+
+def check_length_after_decimal(number):
+    number_str = str(number)
+    # Check if there is a decimal point in the string
+    if 'E-'in number_str:
+        # Split the string at the decimal point and count the length of the part after it
+        length_after_decimal = int(number_str.split('E-')[1])
+    elif '.' in number_str:
+        # Split the string at the decimal point and count the length of the part after it
+        length_after_decimal = len(number_str.split('.')[1])
+    else:
+        length_after_decimal =  0
+    return length_after_decimal
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from calculator_regular import calculate_regular_chances
 from decimal import Decimal
+from components import *
 
 # Main loop to accept user input and perform calculations
 while(True):
@@ -10,12 +11,10 @@ while(True):
         if(occurence_chance == 0):
             print("It is impossible for the event to occur.")
             continue
-        #target_percentage = int((input("Please enter the target likelihood percentage.\n")))
+            
+        target_percentage = int((input("Please enter the target likelihood percentage.\n")))
         
         
+        calculate_regular_chances(occurence_chance, target_percentage)
+        continue
         
-        if(occurence_chance >= Decimal("5E-29")):
-            calculate_regular_chances(occurence_chance, 30)
-            continue
-        else:
-            print("The probability given is too small to calculate.")
